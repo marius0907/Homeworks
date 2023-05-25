c824382d096ec9539c053bfa351924ffa26eb19e
marius0907 <marius.ionita0907@gmail.com>
2023-05-26 00:10:46 +0300

    rewrite class TestShape

--- Homeworks/src/homework2/TestShape.java
+++ Homeworks/src/homework2/TestShape.java
@@ -1,4 +1,10 @@
 package homework2;
 
 public class TestShape {
+
+    public static void main(String[] args) {
+        Shape patrat = new Shape(8);
+        Shape dreptunghi = new Shape(4,2);
+        Shape cerc = new Shape(3.0);
+    }
 }
